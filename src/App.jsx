4f3bdadcbc67b434deabf14302b93d5118f5d704
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-01-14 16:00:24 +0200

    Кінець лекції №1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
+
+
 function App() {
 
+const feedback = [
+  {id: 1, rating: 1, text: "Text 1"},
+  {id: 2, rating: 1, text: "Text 2"},
+  {id: 3, rating: 1, text: "Text 3"}
+]
 
   return (
-    <>
+    <div>
       Привіт, Всесвіте!
-    </>
+      <p>
+{JSON.stringify(feedback)}
+
+      </p>
+    </div>
   )
 }
 
