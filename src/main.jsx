0b51e1e7542dd329acd8a77a00452f77a842b1c6
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-03-18 15:53:00 +0200

    pre public

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import './index.css'
+import './app.css'
 import App from './App.jsx'
 import ErrorPage from "./pages/error-page.jsx";
 import RootPage from "./pages/root-page.jsx";
--- src/pages/root-page.jsx
+++ src/pages/root-page.jsx
@@ -5,12 +5,29 @@ const RootPage = () => {
     return (
         <div>
 
-            <Card>
-                <NavLink to={'/'} activeclassname={'acive'}>Home</NavLink>
-                <NavLink to={'/app'}>App</NavLink>
-                <NavLink to={'/about'}>About</NavLink>
-                <NavLink to={'/params'}>Демо параметрів</NavLink>
-            </Card>
+            <div className="navbar bg-grey-200 shadow-sm">
+                <div className="navbar-start">
+                    <div className="flex-1">
+                        <a className="btn btn-ghost text-xl">WEB</a>
+                    </div>
+                </div>
+
+                <div className="navbar-center flex justify-between">
+                    <NavLink to={'/'} activeclassname={'acive'}>Home</NavLink>
+                    <NavLink to={'/app'}>App</NavLink>
+                    <NavLink to={'/about'}>About</NavLink>
+                    <NavLink to={'/params'}>Демо параметрів</NavLink>
+                </div>
+
+
+            </div>
+
+            {/*<Card>*/}
+            {/*    <NavLink to={'/'} activeclassname={'acive'}>Home</NavLink>*/}
+            {/*    <NavLink to={'/app'}>App</NavLink>*/}
+            {/*    <NavLink to={'/about'}>About</NavLink>*/}
+            {/*    <NavLink to={'/params'}>Демо параметрів</NavLink>*/}
+            {/*</Card>*/}
             <Outlet />
         </div>
     );
